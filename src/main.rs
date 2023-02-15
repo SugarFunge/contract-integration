0a520092e0ed5e7fc080055b4a119d1b223a55b1
MauricinhoMorales <mauricio1999morales@gmail.com>
2023-02-15 15:55:19 -0400

    [Feature] Able to connect to a Wallet and make write calls

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use std::env;
 use ethers_contract::Contract;
 use ethers_core::{
     abi::{Abi, Uint},
+    k256::{
+        ecdsa::SigningKey,
+        pkcs8::{AlgorithmIdentifier, ObjectIdentifier, PrivateKeyInfo},
+    },
     types::{Address, H256, U256},
-    k256::{ecdsa::SigningKey, pkcs8::{PrivateKeyInfo, AlgorithmIdentifier, ObjectIdentifier}},
 };
-use ethers_providers::GOERLI;
-use ethers_signers::{LocalWallet, Wallet, MnemonicBuilder, coins_bip39::English};
+use ethers_middleware::signer::SignerMiddleware;
+use ethers_providers::{Http, Middleware, Provider, GOERLI};
+use ethers_signers::{coins_bip39::English, LocalWallet, MnemonicBuilder, Signer, Wallet};
 use eyre::Result;
+use std::env;
 
-mod abi;
+mod abi_file;
 mod calls;
 mod types;
 
@@ -19,9 +23,6 @@ async fn main() -> Result<()> {
     let contract_address = "0x5209A9A17e0A54615D3C24C92570fB5b9B14AB1b".parse::<Address>()?;
 
     let account_address = "0xbE6157bC090536ee15763356Ac11be00b15951E3".parse::<Address>()?;
-
-    let abi = abi::init().abi;
-    println!("1. OBTENIDO EL ABI");
 
     //-------------------------------------------SUCCESSFULL TESTS-------------------------------------------------------
 
@@ -42,34 +43,33 @@ async fn main() -> Result<()> {
 
     //--------------------------------------------TESTING HOW CAN WE MADE SIGN TRANSACTIONS WITH METAMASK ACCOUNT---------------------
 
-    let contract = Contract::new(contract_address, abi, GOERLI.provider());
+    let abi = abi_file::init().abi;
+    println!("1. OBTENIDO EL ABI");
+
+    let provider = GOERLI.provider();
+    let wallet: LocalWallet =
+        "a0554bccb5a4cbd6e2f74754f66deee5410b67806361e629d1a71299abc8f6db".parse()?;
+
+    let wallet = wallet.with_chain_id(5 as u64);
+
+    let client = SignerMiddleware::new(provider, wallet);
+
+    let ba = client.get_chainid().await.unwrap();
+    println!("EL CHAIN ID DEL CLIENT ES: {:#?}", ba);
+
+    let contract = Contract::new(contract_address, abi, client);
     println!("2. CREADO EL CONTRATO");
 
     // Non-constant methods are executed via the `send()` call on the method builder.
-    let call = contract
-        .method::<_, (Address, U256)>("mintTo", (account_address, U256::from(100000000)))?;
+    let mut tx =
+        contract.method::<_, (Address, U256)>("mintTo", (account_address, U256::from(10000000)))?;
 
-    let algorithm = AlgorithmIdentifier{ 
-      oid: ObjectIdentifier::new_unwrap("0x0"),
-      parameters: None
-    };
+    tx.tx.set_chain_id(5);
 
-    let private_key = env::var("PRIVATE_KEY")?;
+    let pending_tx = tx.send().await?;
 
-    let private_key_info = PrivateKeyInfo::new(algorithm, private_key.as_bytes());
-    let signature = SigningKey::try_from(private_key_info).unwrap();
-
-    let wallet = Wallet::from(signature);
-
-    // let wallet = MnemonicBuilder::<English>::default()
-    // .phrase("aware butter obscure loop case private math wage buyer job enjoy winner")
-    // .build()?;
-
-    wallet.sign_transaction_sync(&call.tx);
-
-    let pending_tx = call.send().await?;
-
-    // `await`ing on the pending transaction resolves to a transaction receipt
+    // You can `await` on the pending transaction to get the receipt with a pre-specified
+    // number of confirmations
     let receipt = pending_tx.confirmations(6).await?;
 
     println!("3. OBTENIDA LA RESPUESTA DEL ENDPOINT: {:#?}", receipt);
